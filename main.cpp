f48aabdf7eada2f8de87ee4c2f208070831750e2
artemgon <artemhoncharuk123@gmail.com>
2024-11-24 17:22:39 +0200

    PE is done.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
+	string s;
+	bool condition = true;
+	cout << "Enter a string (size must be even):" << endl;
+	do
+	{
+		getline(cin, s);
+		cout << endl;
+	} while (s.size() % 2 != 0);
+	for (int i = 0, j = s.size() - 1; i < s.size() / 2 && j > s.size()/2; i++, j--)
+	{
+		if(s.at(i) != s.at(j) && tolower(s.at(i)) != tolower(s.at(j)))
+		{ 
+			condition = false;
+			break;
+		}
+	}
+	cout << "The condition is " << bool(condition) << endl;
 	return 0;
 }
