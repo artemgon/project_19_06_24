2b37dda0389bc3a83321a01b9c61db72dabc3dfe
artemgon <artemhoncharuk123@gmail.com>
2024-11-24 16:15:35 +0200

    Task 2 is done.

--- libs.h
+++ libs.h
@@ -7,5 +7,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <cstdarg>
+#include <cctype>
 
 using namespace std;
--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
+	string s;
+	cout << "Please, enter a string" << endl;
+	getline(cin, s);
+	int i_counter = 0, c_counter = 0, o_counter = 0;
+	for (char& c : s) {
+		if (isdigit(c))
+			i_counter++;
+		else if (isalpha(c))
+			c_counter++;
+		else
+			o_counter++;
+	}
+	cout << "Digits: " << i_counter << endl;
+	cout << "Characters: " << c_counter << endl;
+	cout << "Other symbols: " << o_counter << endl;
 	return 0;
 }
