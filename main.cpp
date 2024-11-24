47136bff56743c65f69d5a78468655b83d5a5792
artemgon <artemhoncharuk123@gmail.com>
2024-11-24 17:34:33 +0200

    Task 1 is done.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 
 int main()
 {
+	string s;
+	int n;
+	cout << "Please, enter a string" << endl;
+	getline(cin, s);
+	cout << "Enter ASCII number for symbol(34-255):" << endl;
+	do
+	{
+		cin >> n;
+		cout << endl;
+	} while (n > 255 || n < 34);
+	char symbol = char(n);
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == symbol) {
+			s.erase(i, 1);
+			i--; 
+		}
+	}
+	cout << "The result is " << s << endl;
 	return 0;
 }
