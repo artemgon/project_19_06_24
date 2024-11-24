f67667bebc3075d61058d95041d8e078dc2051eb
artemgon <artemhoncharuk123@gmail.com>
2024-11-24 16:01:59 +0200

    Task 1 is done.

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
+	string s;
+	cout << "Please, enter a string" << endl;
+	getline(cin, s);
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == ' ')
+		{
+			s[i] = '\t';
+		}
+	}
+	cout << s << endl;
 	return 0;
 }
